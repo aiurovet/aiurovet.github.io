7de2393ace36ed5e0f7f4dc03661c9792560830f
Alexander Iurovetski <aiurovet@gmail.com>
2026-09-06 16:12:20 +1000

    fix: enable tour/theme animations on touch via base rules instead of global transition kill; bump to v39.7

--- anthony/anthkeys/sw.js
+++ anthony/anthkeys/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'anthkeys-v39.6';
+const CACHE = 'anthkeys-v39.7';
 const URLS = ['anthkeys.html', '404.html', 'manifest.json', 'js/mqtt.min.js', 'js/qrcode.js', 'icon-192.png', 'icon-512.png', 'icon-maskable-192.png', 'icon-maskable-512.png', 'apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
